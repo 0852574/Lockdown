b3fff83440b44ab08bf997f93fd147f2739c30b7
Cindy <0852574@schoolsnet.act.edu.au>
2026-06-15 09:45:09 +1000

    Small script error

--- Scripts/lobby.gd
+++ Scripts/lobby.gd
@@ -228,24 +228,15 @@ var active_instance: Node = null
 
 func _GUI_window_open(_body: Player) -> void:
 	if _body.is_multiplayer_authority():
-	var minitask = preload("res://gameMechanics/hacking_minitask.tscn").instantiate()
-	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE # Release mouse
-	Global.taskMode = true
-	GUI.show()
-	GUI_viewport.add_child(minitask)
-	print("player interacted with minitask")
-	if GUI_window != null:
-		GUI_window.emit_signal("close_requested")
-		Global.taskMode = false
-
-
-func _on_Quit_button_pressed() -> void:
-	if player.is_multiplayer_authority():
+		var minitask = preload("res://gameMechanics/hacking_minitask.tscn").instantiate()
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE # Release mouse
 		Global.taskMode = true
 		GUI.show()
 		GUI_viewport.add_child(minitask)
 		print("player interacted with minitask")
+		if GUI_window != null:
+			GUI_window.emit_signal("close_requested")
+			Global.taskMode = false
 
 # player quits window
 		if GUI_window != null:
